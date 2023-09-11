041b036083016fd1e36b82a5441e171b83cc99fe
Tarık <trkefe08@gmail.com>
2023-09-11 21:38:56 +0300

    TermOfUseVC' ye webview eklendi ve sayfa tamamlandı

--- TravioApp kopyası/TravioApp/TravioApp/Scene/MainTabBar/MenuPage/My Added Places/Term of Use/TermOfUseVC.swift
+++ TravioApp kopyası/TravioApp/TravioApp/Scene/MainTabBar/MenuPage/My Added Places/Term of Use/TermOfUseVC.swift
@@ -6,70 +6,86 @@
 //
 
 import UIKit
+import WebKit
+import TinyConstraints
 
-class TermOfUseVC: UIViewController {
-    
-    private lazy var retangle:CustomBackgroundRetangle = {
+final class TermOfUseVC: UIViewController {
+    //MARK: - Views
+    private lazy var rectangleView:CustomBackgroundRetangle = {
         let view = CustomBackgroundRetangle()
-        
         return view
     }()
     
     private lazy var backButton:UIButton = {
-       let button = UIButton()
+        let button = UIButton()
         button.setImage(UIImage(named: "Vector"), for: .normal)
         button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-
+        
         return button
     }()
-
+    
     private lazy var header:UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.text = "Term of Use"
         label.font = Font.poppins(fontType: 600, size: 32).font
         label.textColor = .white
-
+        
         return label
     }()
     
+    private lazy var webView: WKWebView = {
+        let web = WKWebView()
+        web.translatesAutoresizingMaskIntoConstraints = false
+        return web
+    }()
+    let dispatchGroup = DispatchGroup()
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        webView.layoutIfNeeded()
+        self.webView.roundCorners(corners: .topLeft, radius: 40)
+        loadWebsite(urlString: "https://bilgeadamtechnologies.com/privacy-policy/")
+    }
+    //MARK: - Functions
+    private func loadWebsite(urlString: String) {
+        dispatchGroup.enter()
+        DispatchQueue.global().async {
+            if let url = URL(string: urlString) {
+                let request = URLRequest(url: url)
+                DispatchQueue.main.async {
+                    self.webView.load(request)
+                    self.dispatchGroup.leave()
+                }
+            }
+        }
+    }
+    
+    private func setupViews(){
+        self.view.backgroundColor = ColorEnum.travioBackground.uiColor
+        self.view.addSubviews(rectangleView,backButton,header)
+        self.rectangleView.addSubviews(webView)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        backButton.topToSuperview(offset: 36, usingSafeArea: true)
+        backButton.leadingToSuperview(offset: 20)
+        backButton.height(21.5)
+        backButton.width(24)
 
-        setupView()
+        header.leadingToTrailing(of: backButton, offset: 24)
+        header.topToSuperview(offset: 23, usingSafeArea: true)
+        
+        rectangleView.topToBottom(of: header, offset: 54)
+        rectangleView.leadingToSuperview()
+        rectangleView.trailingToSuperview()
+        rectangleView.bottomToSuperview()
+        
+        webView.edgesToSuperview()
     }
     
     @objc func backButtonTapped() {
         navigationController?.popViewController(animated: true)
     }
-    
-    func setupView(){
-        self.view.backgroundColor = ColorEnum.travioBackground.uiColor
-        
-        view.addSubviews(retangle,backButton,header)
-        setupLayouts()
-    }
-    
-    func setupLayouts() {
-        
-        backButton.snp.makeConstraints { make in
-            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(36)
-            make.leading.equalToSuperview().offset(20)
-            make.height.equalTo(21.5)
-            make.width.equalTo(24)
-        }
-
-        header.snp.makeConstraints { make in
-            make.leading.equalTo(backButton.snp.trailing).offset(24)
-            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(23)
-        }
-
-        retangle.snp.makeConstraints { make in
-            make.top.equalTo(header.snp.bottom).offset(54)
-            make.leading.trailing.bottom.equalToSuperview()
-        }
-
-        
-    }
-    
-    
 }
